fc930d16ecfd898886cc44d498a1a68d2b74a6ef
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-04 18:07:03 -0300

    req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    number_of_python_string = count_ocurrences('src/jobs.csv', 'python')
+    
+    assert number_of_python_string == 1639
+
+    number_of_javascript_string = count_ocurrences('src/jobs.csv', 'javascript')
+
+    assert number_of_javascript_string == 122
