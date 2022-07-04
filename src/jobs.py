d858da446acef8190574f2df076c1d196551041d
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-04 18:06:16 -0300

    req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,6 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, encoding="utf-8") as file:
+        reading_file = csv.DictReader(file)
+        return list(reading_file)
